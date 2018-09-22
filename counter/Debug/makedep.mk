ca6b4a8db472e3c047ac2756404ed93ca9c7e5a3
G.Raf <sunriax@live.com>
2018-09-22 21:20:53 +0200

    Update
    Update broken makefile

--- counter/Debug/makedep.mk
+++ counter/Debug/makedep.mk
@@ -2,9 +2,5 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-counter16.c
-
-counter8.c
-
 counter.c
 
